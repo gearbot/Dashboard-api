dc7747b527359a41ade4df3e7008fd8e0e2662e9
AEnterprise <aenterprise2@gmail.com>
2019-05-20 16:24:00 +0200

    fix redis link

--- api.py
+++ api.py
@@ -25,7 +25,7 @@ redis_link = None
 async def get_redis():
     global redis_link
     if redis_link is None:
-        redis_link = await aioredis.create_redis_pool(("192.168.0.128", 6379), encoding="utf-8", db=0,
+        redis_link = await aioredis.create_redis_pool(("localhost", 6379), encoding="utf-8", db=0,
                                                       maxsize=2)  # size 2: one send, one receive
     return redis_link
 
