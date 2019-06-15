f558450b41a7fe76dbf9e86e0f94ddedfc18e461
AEnterprise <aenterprise2@gmail.com>
2019-06-15 21:46:09 +0200

    add /api/redirect route

--- routers/main.py
+++ routers/main.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from starlette.responses import RedirectResponse
 
 router = APIRouter()
 
@@ -6,3 +7,7 @@ router = APIRouter()
 @router.get("/")
 async def read_root():
     return {"status": "WIP"}
+
+@router.get("/redirect")
+async def test():
+    return RedirectResponse(f"https://discordapp.com/api/oauth2/authorize?client_id=365497403928870914&response_type=code&scope=identify guilds&redirect_uri=http://localhost:8000/api/discord/callback/", status_code=307)
