7f376a9784731d6e5739bdef1a0ff3f55e9b9f3b
trilleplay <trille@trilleplay.net>
2019-06-18 17:32:35 +0200

    stop all the hackermans out there from hijacking through CSRF.
    i deserve a star after using fastapi and not having an emotional breakdown.

--- routers/discord.py
+++ routers/discord.py
@@ -1,23 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Cookie
 from starlette.responses import RedirectResponse
 from starlette.requests import Request
-from Utils.Configuration import CLIENT_ID, REDIRECT_URI, CLIENT_URL
+from Utils.Configuration import CLIENT_ID, REDIRECT_URI, CLIENT_URL, API_LOCATION
+from Utils.state import key
+from Utils import Auth
 
 router = APIRouter()
-
-from Utils import Auth
-from Utils.Configuration import API_LOCATION
 
 # Code Generation
 @router.get("/login")
 async def discord_oauth_redir():
-    return RedirectResponse(
-        f"{API_LOCATION}/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&scope=identify guilds",
+    state_key = str(key())
+    response = RedirectResponse(
+        f"{API_LOCATION}/oauth2/authorize?client_id={CLIENT_ID}&state={state_key}&redirect_uri={REDIRECT_URI}&response_type=code&scope=identify guilds",
         status_code=307
     )
+    response.set_cookie(key="state_key", value=state_key)
+    return response
 
 # Callback handling
 @router.get("/callback")
-async def handle_callback(code: str, request: Request):
+async def handle_callback(code: str, state: str, request: Request, state_key: str = Cookie(None)):
+    if state != state_key:
+        return RedirectResponse(f"https://i.imgur.com/vN5jG9r.mp4")
     await Auth.get_bearer_token(request=request, auth_code=code)
     return RedirectResponse(f"{CLIENT_URL}/pleaseclosethispopupformektxh", status_code=307)
