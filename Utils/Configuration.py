e7df9534eef540d2413e5608bc2f0bbac80e7f97
BlackHoleFox <blackholefoxdev@gmail.com>
2019-06-19 02:20:48 -0500

    Progress towards serverstats

--- Utils/Configuration.py
+++ Utils/Configuration.py
@@ -3,10 +3,15 @@ import json
 with open("config.json") as config_file:
     config = json.load(config_file)
 
-CLIENT_ID = config["clientID"]
-CLIENT_SECRET = config["clientSecret"]
-REDIRECT_URI = config["redirect_uri"]
-CLIENT_URL = config["client_url"]
-SESSION_TIMEOUT_LEN = config["session_timeout_length"] * 60*60*24
-SESSION_KEY = config["session_key"]
+CLIENT_ID: int = config["clientID"]
+CLIENT_SECRET: str = config["clientSecret"]
+REDIRECT_URI: str = config["redirect_uri"]
+CLIENT_URL: str = config["client_url"]
+SESSION_TIMEOUT_LEN: int = config["session_timeout_length"] * 60*60*24
+SESSION_KEY: str = config["session_key"]
 API_LOCATION = "https://discordapp.com/api/v6"
+
+__REDIS_PARTS = config["redis_address"]
+REDIS_ADDRESS = (__REDIS_PARTS[0], __REDIS_PARTS[1])
+CORS_ORGINS: list = config["cors_orgins"]
+TRUSTED_HOSTS: list = config["trusted_hosts"]
--- routers/main.py
+++ routers/main.py
@@ -6,6 +6,9 @@ router = APIRouter()
 
 from Utils import Auth, Redis
 
+from time import perf_counter_ns
+
+bad_request_resp = JSONResponse({ "status": "BAD REQUEST"}, status_code=400)
 
 @router.get("/")
 async def read_root():
@@ -23,12 +26,6 @@ async def get_test(request: Request):
     return {"test": request.session["test"]}
 
 
-@router.get("/whoami")
-@Auth.auth_required
-async def identify_endpoint(request: Request):
-    return await Redis.ask_the_bot("user_info", user_id=request.session["user_id"])
-
-
 @router.get("/logout")
 @Auth.auth_required
 async def logout(request: Request):
@@ -37,15 +34,39 @@ async def logout(request: Request):
     return JSONResponse(dict(status="Success"))
 
 
+@router.get("/whoami")
+@Auth.auth_required
+async def identify_endpoint(request: Request):
+    return await Redis.ask_the_bot("user_info", user_id=request.session["user_id"])
+
 @router.get("/guilds")
 @Auth.auth_required
 async def guild_list_endpoint(request: Request):
     # Grab the user's guilds from Discord
-    guilds_list = await Auth.query_endpoint(request, "get", "/users/@me/guilds")
 
-    formatted_guild_list = []
+    start_time = perf_counter_ns()
 
-    for guild in guilds_list:
-        formatted_guild_list.append(guild["id"])
+    return_value = await Redis.ask_the_bot("guild_perms", user_id=request.session["user_id"])
 
-    return await Redis.ask_the_bot("guild_perms", user_id=request.session["user_id"], guild_list=formatted_guild_list)
+    finish_time = perf_counter_ns()
+    final_time = (finish_time - start_time) / 1000000
+    print("The request took a total of: " + str(final_time))
+
+    return return_value
+
+@router.get("/guilds/{guild_id}/stats")
+async def guild_stats_endpoint(guild_id: int, request: Request):
+    print(request)
+    print(guild_id)
+    return "Made it"
+    if guild_id == None:
+        return bad_request_resp
+    
+    server_info = await Redis.ask_the_bot("guild_info", 
+        user_id=request.session["user_id"],
+        guid=guild_id
+    )
+
+    print(server_info)
+
+    return server_info
