928a350d09bc56e686b4b00840509b9c5b1ac044
BlackHoleFox <blackholefoxdev@gmail.com>
2019-06-24 22:39:30 -0500

    Handle when user denies OAuth

--- routers/discord.py
+++ routers/discord.py
@@ -15,13 +15,21 @@ async def discord_oauth_redir():
         f"{API_LOCATION}/oauth2/authorize?client_id={CLIENT_ID}&state={state_key}&redirect_uri={REDIRECT_URI}&response_type=code&scope=identify guilds",
         status_code=307
     )
-    response.set_cookie(key="state_key", value=state_key)
+    # We won't need this cookie 5 minutes later
+    response.set_cookie(key="state_key", value=state_key, expires=300)
     return response
 
 # Callback handling
 @router.get("/callback")
-async def handle_callback(code: str, state: str, request: Request, state_key: str = Cookie(None)):
+async def handle_callback(error: str=None, code: str=None, state: str=None, request: Request=None, state_key: str = Cookie(None)):
+    if error != None: # They must of denied the auth request
+        return RedirectResponse(f"{CLIENT_URL}")
+
     if state != state_key:
         return RedirectResponse("https://i.imgur.com/vN5jG9r.mp4")
-    await Auth.get_bearer_token(request=request, auth_code=code)
-    return RedirectResponse(f"{CLIENT_URL}/pleaseclosethispopupformektxh", status_code=307)
+
+    if code != None:
+        await Auth.get_bearer_token(request=request, auth_code=code)
+        return RedirectResponse(f"{CLIENT_URL}/pleaseclosethispopupformektxh", status_code=307)
+    else:
+        return RedirectResponse("https://i.imgur.com/vN5jG9r.mp4")
