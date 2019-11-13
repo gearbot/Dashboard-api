11b28136c377985467bc92b917d15a3babf31421
AEnterprise <aenterprise2@gmail.com>
2019-11-13 22:10:00 +0100

    allow changing per_page

--- routers/websocket/infractions.py
+++ routers/websocket/infractions.py
@@ -4,11 +4,15 @@ from tortoise.query_utils import Q
 from Utils.DataModels import Infraction
 
 
-async def inf_search(websocket, question=None, guild_id=None, page=1, order_by=None):
+async def inf_search(websocket, question=None, guild_id=None, page=1, order_by=None, per_page=10):
     if order_by is None:
         order_by = ["-id"]
     if not str(guild_id).isnumeric():
         return dict(error=True, message="Not a guild id")
+    if not isinstance(per_page, int):
+        return dict(error=True, message="please send me an actual page limit")
+    if per_page < 10 or per_page > 100:
+        return dict(error=True, message="per_page must be between 10 and 100")
     if websocket.auth_info is None or "guild_infractions" not in websocket.active_subscriptions or \
             websocket.active_subscriptions["guild_infractions"] != guild_id:
         return dict(error=True, message="Unauthorized")
@@ -17,12 +21,14 @@ async def inf_search(websocket, question=None, guild_id=None, page=1, order_by=N
     filter = Q(guild_id=guild_id)
 
     count = await Infraction.filter(filter).count()
+    if (page - 1) * per_page > count:
+        return dict(error=True, message="Invalid page")
     try:
         infractions = await Infraction\
             .filter(filter)\
             .order_by(*order_by)\
-            .offset((page - 1) * 50)\
-            .limit(50)
+            .offset((page - 1) * per_page)\
+            .limit(per_page)
     except FieldError as error:
         return dict(error=True, message=str(error))
 
