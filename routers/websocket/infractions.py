fa894ddb795a690db632f454319b90551c91ad24
AEnterprise <aenterprise2@gmail.com>
2019-11-17 16:33:59 +0100

    basic infraction filtering

--- routers/websocket/infractions.py
+++ routers/websocket/infractions.py
@@ -4,6 +4,31 @@ from tortoise.query_utils import Q
 from Utils.DataModels import Infraction
 
 
+def field_concat(prefix):
+    def _actual(field_name):
+        return f"{field_name}__{prefix}"
+
+    return _actual
+
+
+CONVERTERS = {
+    "GREATER_THEN": field_concat("gt"),
+    "GREATER_OR_EQUAL_THEN": field_concat("gte"),
+    "SMALLER_THEN": field_concat("lt"),
+    "SMALLER_OR_EQUAL_THEN": field_concat("lte"),
+    "EQUALS": lambda f: f
+}
+
+
+def assemble_Q(question):
+    # re-assemble Q object
+    direct_filters = {
+        CONVERTERS[s['type']](s['field']): s['value'] for s in question["set"]
+    }
+    filters = [assemble_Q(q) for q in question['subFilters']]
+    return Q(Q(**direct_filters), Q(*filters), join_type=question["mode"])
+
+
 async def inf_search(websocket, question=None, guild_id=None, page=1, order_by=None, per_page=10):
     if order_by is None:
         order_by = ["-id"]
@@ -16,18 +41,20 @@ async def inf_search(websocket, question=None, guild_id=None, page=1, order_by=N
     if websocket.auth_info is None or "guild_infractions" not in websocket.active_subscriptions or \
             websocket.active_subscriptions["guild_infractions"] != guild_id:
         return dict(error=True, message="Unauthorized")
+
     guild_id = int(guild_id)
+    guild_Q = Q(guild_id=guild_id)
 
-    filter = Q(guild_id=guild_id)
+    master_filter = Q(guild_Q, assemble_Q(question), join_type=Q.AND)
 
-    count = await Infraction.filter(filter).count()
+    count = await Infraction.filter(master_filter).count()
     if (page - 1) * per_page > count:
         return dict(error=True, message="Invalid page")
     try:
-        infractions = await Infraction\
-            .filter(filter)\
-            .order_by(*order_by)\
-            .offset((page - 1) * per_page)\
+        infractions = await Infraction \
+            .filter(master_filter) \
+            .order_by(*order_by) \
+            .offset((page - 1) * per_page) \
             .limit(per_page)
     except FieldError as error:
         return dict(error=True, message=str(error))
